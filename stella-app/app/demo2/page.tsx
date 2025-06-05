3fcfe3e5d7bac6284dda1e819ae689d7849fe144
To5pl <mariastella.merra@itisandria.it>
2025-06-05 14:36:34 +0200

    lezione 3.1

--- stella-app/app/demo2/page.tsx
+++ stella-app/app/demo2/page.tsx
@@ -1,3 +1,6 @@
+import { User } from '@/model/user';
+
+
 export const metadata={
     title: 'Demo 2',
     description: 'This is the Demo 2 page',
@@ -8,10 +11,22 @@ export const metadata={
 }
 
 
-export default function Demo2(){
-    return (
-        <main className="page">
+async function getData(): Promise<User[]> {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  return res.json();
+}
+
+export default async function Demo2() {
+  const data = await getData();
+
+  return (
+    <main>
       <h1>Demo 2</h1>
+      {
+        data?.map(user => {
+          return <li key={user.id}>{user.name}</li>
+        })
+      }
     </main>
-    )
+  )
 }
