718032a2cce764a9f397a5a540860d65bf32a3d1
To5pl <mariastella.merra@itisandria.it>
2025-06-05 16:27:32 +0200

    lezione 4.5

--- stella-app/app/demo3/page.tsx
+++ stella-app/app/demo3/page.tsx
@@ -7,8 +7,9 @@ export default function Demo3() {
     <main>
       <div>
         <h1>Demo 3</h1>
-        <ClientComponent />
-        <ServerComponent />
+        <ClientComponent>
+          <ServerComponent />
+        </ClientComponent>
       </div>
     </main>
   )
