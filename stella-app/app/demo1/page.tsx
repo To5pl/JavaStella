67d37ad01019222522ecacfc9d197a5ae59576c6
To5pl <mariastella.merra@itisandria.it>
2025-06-05 11:42:51 +0200

    lezione 2.2

--- stella-app/app/demo1/page.tsx
+++ stella-app/app/demo1/page.tsx
@@ -1,3 +1,12 @@
+export const metadata={
+    title: 'Demo 1',
+    description: 'This is the Demo 1 page',
+    openGraph:{
+        title: 'Demo 1',
+        description: '...'
+    }
+}
+
 export default function Demo1(){
     return (
         <main className="page">
--- stella-app/app/demo2/page.tsx
+++ stella-app/app/demo2/page.tsx
@@ -1,3 +1,13 @@
+export const metadata={
+    title: 'Demo 2',
+    description: 'This is the Demo 2 page',
+    openGraph:{
+        title: 'Demo 2',
+        description: 'bla bla...'
+    }
+}
+
+
 export default function Demo2(){
     return (
         <main className="page">
