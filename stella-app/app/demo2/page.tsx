456a88a9d2a7c189adec6ccbabb74b4c1d6cfe74
To5pl <mariastella.merra@itisandria.it>
2025-06-05 11:35:30 +0200

    lezione 2.1

---
+++ stella-app/app/demo2/page.tsx
@@ -0,0 +1,7 @@
+export default function Demo2(){
+    return (
+        <main className="page">
+      <h1>Demo 2</h1>
+    </main>
+    )
+}
--- stella-app/app/page.tsx
+++ stella-app/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="page">
       <h1>Hello Next</h1>
     </main>
   )
