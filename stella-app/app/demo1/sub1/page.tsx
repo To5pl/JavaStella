2c02151d8786e8197c66a379cecf5a2317b0b3b4
To5pl <mariastella.merra@itisandria.it>
2025-06-05 11:58:33 +0200

    lezione 2.4

---
+++ stella-app/app/demo1/sub1/page.tsx
@@ -0,0 +1,7 @@
+export default function Sub1() {
+  return (
+    <div className="page">
+      <h1>Sub1 Page</h1>
+    </div>
+  )
+}
---
+++ stella-app/app/demo1/sub2/page.tsx
@@ -0,0 +1,7 @@
+export default function Sub2() {
+  return (
+    <div className="page">
+      <h1>Sub2 Page</h1>
+    </div>
+  )
+}
